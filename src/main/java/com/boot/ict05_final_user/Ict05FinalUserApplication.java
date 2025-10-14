9410be4bcc6f83433a5800dd26745532dd29840b
Park miri <already0310@gmail.com>
2025-10-14 11:38:23 +0900

    프로젝트 생성

---
+++ src/main/java/com/boot/ict05_final_user/Ict05FinalUserApplication.java
@@ -0,0 +1,13 @@
+package com.boot.ict05_final_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ict05FinalUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ict05FinalUserApplication.class, args);
+	}
+
+}
